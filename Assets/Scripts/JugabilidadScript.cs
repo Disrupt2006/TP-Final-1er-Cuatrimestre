f112a298b302c72c24b1337e03b77fd969ad2b07
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-06-13 10:37:06 -0300

    Comienzo de movimiento de deathwalls

--- Assets/Scripts/JugabilidadScript.cs
+++ Assets/Scripts/JugabilidadScript.cs
@@ -10,7 +10,12 @@ public class JugabilidadScript : MonoBehaviour
     public GameObject DeathCubePrefab;
     public GameObject Confeti;
 
+    public float SpeedDeathCube;
+
     public Text CoinText;
+
+    bool toRight;
+    bool toUp;
 
     int contadorMonedas = 0;
 
@@ -20,12 +25,54 @@ public class JugabilidadScript : MonoBehaviour
     {
         Vector3 randomSpawnPosition = new Vector3(Random.Range(-9, 10), 0.8f, Random.Range(-9, 10));
         Instantiate(Monedaprefab, randomSpawnPosition, Quaternion.identity);
+
+        toRight = true;
+        toUp = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if (contadorMonedas % 2 == 0 && toRight == true)
+        {
+            DeathCubePrefab.transform.Translate(SpeedDeathCube, 0, 0);
+        }
+        else if (toRight == false)
+        {
+            DeathCubePrefab.transform.Translate(-SpeedDeathCube, 0, 0);
+        }
+
+        if (DeathCubePrefab.transform.position.x == 9.9)
+        {
+            toRight = false;
+        }
+        if (DeathCubePrefab.transform.position.x == -9.8)
+        {
+            toRight = true;
+        }
+
         
+        
+        if (contadorMonedas % 2 != 0 && toUp == true)
+        {
+            DeathCubePrefab.transform.Translate(0, 0, SpeedDeathCube);
+        }
+        else if (toUp == false)
+        {
+            DeathCubePrefab.transform.Translate(0, 0, -SpeedDeathCube);
+        }
+
+        if (DeathCubePrefab.transform.position.z == 19.9)
+        {
+            toUp = false;
+        }
+        if (DeathCubePrefab.transform.position.z == 0.18)
+        {
+            toUp = true;
+        }
+        
+
     }
 
     void OnCollisionStay(Collision col)
@@ -43,7 +90,8 @@ public class JugabilidadScript : MonoBehaviour
                 Instantiate(Monedaprefab, randomSpawnPositionCoin, Quaternion.identity);
 
                 Vector3 randomSpawnPositionDeathCube = new Vector3(Random.Range(-9, 10), 0.8f, Random.Range(-9, 10));
-                Instantiate(DeathCubePrefab, randomSpawnPositionDeathCube, Quaternion.identity);
+                DeathCubePrefab = Instantiate(DeathCubePrefab, randomSpawnPositionDeathCube, Quaternion.identity);
+
             }
 
            else
